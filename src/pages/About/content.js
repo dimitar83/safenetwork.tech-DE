41292a3022da4380f2d56041d471680e399c729c
dimitar83 <38901442+dimitar83@users.noreply.github.com>
2020-10-08 11:09:28 +0300

    Update content.js

--- src/pages/About/content.js
+++ src/pages/About/content.js
@@ -1,14 +1,14 @@
 export default {
-  desc: 'The company that originally envisaged the SAFE Network, MaidSafe are driven by one overriding goal: to provide privacy, security and freedom for everyone on the planet.',
+  desc: 'Das MaidSafe Unternehmen, das ursprünglich das SAFE Network ins Auge gefasst hatte, verfolgt ein übergeordnetes Ziel: Datenschutz, Sicherheit und Freiheit für alle Menschen auf dem Planet.',
   CTA: {
-    name: 'Contact MaidSafe',
+    name: 'Wenden Sie sich an MaidSafe',
     url: 'https://maidsafe.net/'
   },
   para: [
-    'In 2006, MaidSafe began work on a decentralised internet that is private, secure and free for everybody. Since those early days, the team, led by founder David Irvine, has been focused solely on building the SAFE Network to solve the problems of an increasingly centralised internet that is facing more issues with each day that passes.',
-    'MaidSafe are based in Ayr, Scotland but the team is distributed all across the globe. It has has drawn talents from many different cultures and countries, reflecting the users that they serve. Comprised of thinkers, inventors, tinkerers, PhDs, engineers and designers, they are building software that is [open source and free to use](https://github.com/maidsafe) and share in a single mission: a desire to provide security and privacy for everyone.',
-    'MaidSafe are guided and influenced by the SAFE Network Community who have helped to fund the work and guide its decisions through discussions across community-run platforms.',
-    'The SAFE Network is currently in Alpha Release stages. Upon full release, MaidSafe will be only one of many thousands of developers making a living by carrying out development on the Network as a result of the built-in incentivisation scheme.',
-    'To contact the team directly, visit the company website at [maidsafe.net](https://maidsafe.net/contact) for further details.'
+    'Im Jahr 2006 begann MaidSafe mit der Arbeit an einem dezentralisierten Internet, das privat, sicher und für alle kostenlos ist. Seit jenen frühen Tagen hat sich das Team unter der Leitung von Gründer David Irvine ausschließlich auf den Aufbau des SAFE Networks konzentriert, um die Probleme eines zunehmend zentralisierten Internets zu lösen, das mit jedem Tag mehr Probleme hat.',
+    'MaidSafe hat seinen Sitz in Ayr, Schottland, aber das Team ist auf der ganzen Welt verteilt. Es hat Talente aus vielen verschiedenen Kulturen und Ländern angezogen, die die Benutzer widerspiegeln, denen sie dienen. Sie besteht aus Denkern, Erfindern, Ingenieuren und Designern, die Software entwickeln, [die Open-Source ist und kostenlos verwendet](https://github.com/maidsafe) sie kann in einer einzigen Aufgabe geteilt werden: dem Wunsch, Sicherheit und Datenschutz für alle zu bieten.',
+    'MaidSafe wird von der SAFE Network Gemeinde geleitet und beeinflusst, die dazu beigetragen hat, die Arbeit zu finanzieren und ihre Entscheidungen durch Diskussionen auf von der Gemeinde betriebenen Plattformen zu steuern.',
+    'Das SAFE Network befindet sich derzeit in der Alphaauslösungsphase. Nach der vollständigen Veröffentlichung wird MaidSafe nur einer von vielen Tausenden von Entwicklern sein, die ihren Lebensunterhalt mit der Entwicklung im Netzwerk als Ergebnis des integrierten Anreizschemas verdienen.',
+    'Um das Team direkt zu kontaktieren, besuchen Sie die Unternehmenswebsite unter [maidsafe.net](https://maidsafe.net/contact) um weitere Informationen zu erhalten.'
   ]
 }
